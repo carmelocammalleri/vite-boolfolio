1a3ccc79c1d1300c4430145acfcb088797866d07
Melozzo <carmecamma1995@gmail.com>
2023-12-06 16:40:29 +0100

    [create]: ViewProjectComponent file

---
+++ src/components/ViewProjectComponent.vue
@@ -0,0 +1,27 @@
+<script>
+
+import {store} from './data/store';
+
+export default{
+  name:'ViewProjectComponent',
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+   
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
