bcbda2a7bec7e72945c9842bd9020806bd9ff8a3
Melozzo <carmecamma1995@gmail.com>
2023-12-10 16:31:05 +0100

    [create]: file About and Home

---
+++ src/pages/About.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name:'About',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+
+    <h1>About</h1>
+    
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name:'Home',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+
+    <h1>Home</h1>
+    
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
